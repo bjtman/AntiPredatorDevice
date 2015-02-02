b098ce4bb413f15b167f0aa62697e8f4e85af459
bjtice <bjtice@gmail.com>
2015-02-02 19:35:29 +0000

    Added support for first PIR in state machine

--- AntiPredatorDevice.ino
+++ AntiPredatorDevice.ino
@@ -224,6 +224,17 @@ long lastRightDebounceTime = 0;  // the last time the output pin was toggled
 long debounceDelay = 200;    // the debounce time
 
 // ++
+// PIR SENSOR GLOBAL CONSTANTS
+//the time when the sensor outputs a low impulse
+long unsigned int lowIn;
+
+//the amount of milliseconds the sensor has to be low
+//before we assume all motion has stopped
+long unsigned int pause = 5000;
+
+boolean lockLow = true;
+boolean takeLowTime;
+
 
 void setup() {
 	
@@ -493,6 +504,45 @@ void loop() {
 					digitalWrite(PIEZO_SOUNDER_PIN,LOW);
 				}
 			}
+			
+			
+			// Check PIR's Here
+			
+			
+			
+			 if(digitalRead(PIR_A_LED_PIN         ) == HIGH){
+				 //digitalWrite(ledPin, HIGH);   //the led visualizes the sensors output pin state
+				 if(lockLow){
+					 //makes sure we wait for a transition to LOW before any further output is made:
+					 lockLow = false;
+					 Serial.println("---");
+					 Serial.print("motion detected at ");
+					 Serial.print(millis()/1000);
+					 Serial.println(" sec");
+					 delay(50);
+				 }
+				 takeLowTime = true;
+			 }
+
+			 if(digitalRead(PIR_A_LED_PIN         ) == LOW){
+				// digitalWrite(ledPin, LOW);  //the led visualizes the sensors output pin state
+
+				 if(takeLowTime){
+					 lowIn = millis();          //save the time of the transition from high to LOW
+					 takeLowTime = false;       //make sure this is only done at the start of a LOW phase
+				 }
+				 //if the sensor is low for more than the given pause,
+				 //we assume that no more motion is going to happen
+				 if(!lockLow && millis() - lowIn > pause){
+					 //makes sure this block of code is only executed again after
+					 //a new motion sequence has been detected
+					 lockLow = true;
+					 Serial.print("motion ended at ");      //output
+					 Serial.print((millis() - pause)/1000);
+					 Serial.println(" sec");
+					 delay(50);
+				 }
+			 }
 			
 			
 			
